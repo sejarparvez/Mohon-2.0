e7d2b467bde937312006bbcc24554491c74c5db3
sejarparvez <sejarparvez@yahoo.com>
2024-10-14 08:32:10 +0600

    added smooth scrolling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,24 @@ import Contact from "@/components/pages/home/contact/Contact";
 import Hero from "@/components/pages/home/hero/Hero";
 import Services from "@/components/pages/home/services/Services";
 import Testimonials from "@/components/pages/home/testimonials/Testimonials";
+import { ReactLenis } from "../lib/react-lenis";
 
 export default function Home() {
+  const lenisOptions = {
+    lerp: 0.07, 
+    duration: 1.5,
+    smoothTouch: true,
+    smooth: true,
+  };
   return (
     <>
-      <Hero />
-      <Services />
-      <Testimonials />
-      <Contact />
-      <Footer />
+      <ReactLenis root options={lenisOptions}>
+        <Hero />
+        <Services />
+        <Testimonials />
+        <Contact />
+        <Footer />
+      </ReactLenis>
     </>
   );
 }
